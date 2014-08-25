541c0e341e8a6df0fe656772dcf8ed7d3c006380
Adam Jimerson <vendion@gmail.com>
2014-08-25 12:29:14 -0400

    Adding error handling
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- gigcity.go
+++ gigcity.go
@@ -9,13 +9,45 @@ func init() {
 	http.HandleFunc("/", rootHandler)
 }
 
+func errorHandler(w http.ResponseWriter, r *http.Request, status int, err string) {
+	w.WriteHeader(status)
+	switch status {
+	case http.StatusNotFound:
+		page := template.Must(template.ParseFiles(
+			"static/_base.html",
+			"static/404.html",
+		))
+
+		if err := page.Execute(w, nil); err != nil {
+			errorHandler(w, r, http.StatusInternalServerError, err.Error())
+			return
+		}
+	case http.StatusInternalServerError:
+		page := template.Must(template.ParseFiles(
+			"static/_base.html",
+			"static/500.html",
+		))
+
+		if err := page.Execute(w, nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		errorHandler(w, r, http.StatusNotFound, "")
+		return
+	}
+
 	page := template.Must(template.ParseFiles(
 		"static/_base.html",
 		"static/index.html",
 	))
 
 	if err := page.Execute(w, nil); err != nil {
-		panic(err)
+		errorHandler(w, r, http.StatusInternalServerError, err.Error())
+		return
 	}
 }
