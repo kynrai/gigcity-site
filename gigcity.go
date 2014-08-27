5c332a4a45ab00e798e8dd79299b0b978a22ea33
Adam Jimerson <vendion@gmail.com>
2014-08-27 17:17:01 -0400

    Adding comments
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- gigcity.go
+++ gigcity.go
@@ -11,12 +11,21 @@ import (
 	"appengine/user"
 )
 
+// Event contains details about GDG events, used when preforming read/write ops
+// to the datastore
 type Event struct {
-	Title    string
+	// Title of the event
+	Title string
+	// Datetime of the event, sent from the browser in YYYY-MM-DDTHH:MM
+	// format
 	Datetime string
-	Details  string
+	// The details about the event
+	Details string
 }
 
+// Under appengine our code runs as a package, not a binary.  Due to this
+// define the routes during package initilization.  Normally this wourd happen
+// with in main()
 func init() {
 	// handle static paths
 	http.Handle("/static/img/", http.StripPrefix("/static/img/", http.FileServer(http.Dir("static/img"))))
@@ -28,6 +37,8 @@ func init() {
 	http.HandleFunc("/", rootHandler)
 }
 
+// Handle errors here, this allows us to control the format of the output rather
+// than using http.Error() defaults
 func errorHandler(w http.ResponseWriter, r *http.Request, status int, err string) {
 	w.WriteHeader(status)
 	switch status {
@@ -48,17 +59,22 @@ func errorHandler(w http.ResponseWriter, r *http.Request, status int, err string
 		))
 
 		if err := page.Execute(w, nil); err != nil {
+			// IF for some reason the tempalets for 500 errors fails, fallback
+			// on http.Error()
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
 }
 
+// Fetches the next index key out of the datastore for the Events entity
 func eventList(c appengine.Context) *datastore.Key {
 	return datastore.NewKey(c, "Events", "default_eventlist", 0, nil)
 }
 
+// Handles requests to '/' as well as any unmatched routes to the server
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	// If the request is not for the root of the app, then it is a 404
 	if r.URL.Path != "/" {
 		errorHandler(w, r, http.StatusNotFound, "")
 		return
@@ -75,6 +91,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Handles requests to /about
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	page := template.Must(template.ParseFiles(
 		"static/_base.html",
@@ -87,15 +104,22 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Admin page to add new event information to the datastore
 func addEventHandler(w http.ResponseWriter, r *http.Request) {
+	// use the request information to determine if this is a new session
 	c := appengine.NewContext(r)
+	// get user information if one is logged in
 	u := user.Current(c)
 	if u == nil {
+		// the person that made the request is anonymous, redirect them to the login
+		// page
 		url, err := user.LoginURL(c, r.URL.String())
 		if err != nil {
+			// was unable to get a login URL, so die with a 500 error
 			errorHandler(w, r, http.StatusInternalServerError, err.Error())
 			return
 		}
+		// set a return URL for when authentication succeeds
 		w.Header().Set("Location", url)
 		w.WriteHeader(http.StatusFound)
 		return
@@ -110,12 +134,16 @@ func addEventHandler(w http.ResponseWriter, r *http.Request) {
 			Details:  r.FormValue("details"),
 		}
 
+		// get the next available index key
 		key := datastore.NewIncompleteKey(c, "Events", eventList(c))
+		// write the data to the datastore
 		_, err := datastore.Put(c, key, &g)
 		if err != nil {
 			errorHandler(w, r, http.StatusInternalServerError, err.Error())
 			return
 		}
+
+		// send the user back to the view page once done
 		http.Redirect(w, r, "/events", http.StatusFound)
 	} else if r.Method == "GET" {
 		// handle get requests
@@ -133,10 +161,15 @@ func addEventHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Handles requests to /events
 func eventHandler(w http.ResponseWriter, r *http.Request) {
+	// use the request information to determine if this is a new session
 	c := appengine.NewContext(r)
+	// query the Events entity in the datastore
 	q := datastore.NewQuery("Events").Ancestor(eventList(c)).Order("-Datetime").Limit(10)
+	// create a slice of Event with a capacity of 10 items
 	events := make([]Event, 0, 10)
+	// store the results into the events slice
 	if _, err := q.GetAll(c, &events); err != nil {
 		errorHandler(w, r, http.StatusInternalServerError, err.Error())
 		return
