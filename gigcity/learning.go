c41f9527c286f8c4a2a5c0a6ff2414198e50fe89
Adam Jimerson <vendion@gmail.com>
2014-11-19 12:26:47 -0500

    Adding start of learn HTML template WIP
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- gigcity/learning.go
+++ gigcity/learning.go
@@ -1,1 +1,18 @@
 package gigcity
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func learningHandler(w http.ResponseWriter, r *http.Request) {
+	page := template.Must(template.ParseFiles(
+		"static/_base.html",
+		"static/learn.html",
+	))
+
+	if err := page.Execute(w, nil); err != nil {
+		errorHandler(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
